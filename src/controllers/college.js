25aba81ed7c046f5b12e6a131ae8ff1a33a5adc6
Mohammad Naseem <niceman1915@gmail.com>
2022-05-04 15:52:11 +0530

    added to get

--- src/controllers/college.js
+++ src/controllers/college.js
@@ -22,6 +22,7 @@ const getColleges = async function(req, res){
         cId = cId._id.toString()
         let part1 = await CollegeModel.findOne({name : cname}).select({_id : 0, name : 1, fullName : 1, logoLink : 1})
         let part2 = await InternModel.find({collegeId : cId}).select({name : 1, email : 1, mobile : 1})
+        if(part2.length == 0) return res.status(404).send({status : false, msg : "No interns at this particular moment"})
         let temp = part1.toJSON()
         temp["interests"] = [...part2]
 
