0e1bcce6c95ba56b00e67329597eb2870ee3a3f8
Achal-jain <achaljain412@gmail.com>
2022-05-07 21:06:47 +0530

    final

--- src/controllers/intern.js
+++ src/controllers/intern.js
@@ -25,16 +25,8 @@ const createIntern = async (req, res) => {
     if (validString.test(intern.name))
       return res.status(400).send({ status: false, msg: "Name must not contains numbers" });
 
-<<<<<<< HEAD
     if(!validateEmail.validate(req.body.email))
      return res.status(400).send({ status: false, msg: "Enter a valid email" })
-=======
-
-  
-    if (!validateEmail.validate(req.body.email))
-      return res.status(400).send({ status: false, msg: "Enter a valid email" })
-
->>>>>>> 20d04380c3b8b9db79efb4b43e54b5c3100bdfd0
 
     let showInternData = await internModel.create(intern);
     res.status(201).send({ status: true, data: showInternData });
