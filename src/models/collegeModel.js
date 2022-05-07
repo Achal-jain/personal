276ccadfd7692a897988417ed04dbb7f887e372d
Achal-jain <achaljain412@gmail.com>
2022-05-07 21:55:09 +0530

    Complete

--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose")
-const ObjectId = mongoose.Schema.Types.ObjectId
 
 const collegeModel = new mongoose.Schema({
     name : {type : String, required : [true, "Pls give a name"], unique : true},
